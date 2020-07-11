dc69024e6de4d0ce154aa075f9f0d460eb0d386b
pfflanders <pfflanders@gmail.com>
2020-07-10 18:58:57 -0700

    Three times the css, three times the fun.
    more css, script to refresh between them.

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-name = prompt("What is your name?");
-document.write(name);
+let imports = [
+    "<link id='framework' rel='stylesheet' type='text/css' href='cat.css' />",
+    "<link id='framework' rel='stylesheet' type='text/css' href='dog.css' />",
+	"<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'	integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk'	crossorigin='anonymous'>"
+];
+
+document.write(imports[Math.floor(Math.random() * 100) % imports.length])
