faaadfcadb75a1fbf3722f2248887920fa052894
pfflanders <pfflanders@gmail.com>
2020-09-07 13:57:39 -0700

    added chinchilla
    just changed colors because I disliked what it was before

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let imports = [
     "<link id='framework' rel='stylesheet' type='text/css' href='cat.css' />",
     "<link id='framework' rel='stylesheet' type='text/css' href='dog.css' />",
+	"<link id='framework' rel='stylesheet' type='text/css' href='chinchilla.css' />",
 ];
 
 document.write(imports[Math.floor(Math.random() * 100) % imports.length])
